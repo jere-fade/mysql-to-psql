8929eb4083e7a6cf43ec9fd0b17efb70273467f7
jeremy <jeremyhuacs@gmail.com>
2023-05-30 15:09:44 +0800

    write row complete

--- write.go
+++ write.go
@@ -3,16 +3,14 @@ package main
 import (
 	"fmt"
 	"strings"
-	"database/sql"
+
 	"github.com/go-mysql-org/go-mysql/replication"
-	// "github.com/lib/pq"
 )
 
 func (pc *PsqlConn) processWriteRow(re *replication.RowsEvent) error {
 	writeValues := re.Rows
 	tableName := string(re.Table.Table)
 	sqlSlice := make([]string, len(writeValues))
-	schema := string(re.Table.Schema)
 
 	for i := 0; i < len(writeValues); i++ {
 		stringValues := make([]string, len(writeValues[i]))
@@ -27,33 +25,9 @@ func (pc *PsqlConn) processWriteRow(re *replication.RowsEvent) error {
 		}
 		sqlSlice[i] = "INSERT INTO " + tableName + " VALUES (" + strings.Join(stringValues, ", ") + ");"
 	}
-	if pc.pq_dbname != schema {
-		pc.conn.Close()
-		// close old connection, create new connection to psql using 'schema'
-		pc.pq_dbname = schema
-		psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-			pq_host,
-			pq_port,
-			pq_user,
-			pq_password,
-			pc.pq_dbname,
-		)
-		var err error
-		pc.conn, err = sql.Open("postgres", psqlconn)
-		if err != nil {
-			return err
-		}
-	} else {
-		// use current connection
-		pingErr := pc.conn.Ping()
-		if pingErr != nil {
-			return pingErr
-		}
-	}
 
 	for i := 0; i < len(sqlSlice); i++ {
-		// _, err := pc.conn.Exec(sqlSlice[i])
-		_, err := pc.conn.Exec("SHOW search_path;select * from student;")
+		_, err := pc.conn.Exec(sqlSlice[i])
 		fmt.Println(sqlSlice[i])
 		if err != nil {
 			return err

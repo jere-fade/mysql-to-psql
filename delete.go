ed1eb8229a471c4bb4a7fa26e010057ef7203b11
jeremy <jeremyhuacs@gmail.com>
2023-06-02 21:03:17 +0800

    process delete row event complete

--- delete.go
+++ delete.go
@@ -1,7 +1,52 @@
 package main
 
-import "github.com/go-mysql-org/go-mysql/replication"
+import (
+	"fmt"
+
+	"github.com/go-mysql-org/go-mysql/replication"
+)
 
 func (pc *PsqlConn) processDeleteRow(re *replication.RowsEvent) error {
+	tableName := string(re.Table.Table)
+	columnNames := make([]string, 0)
+
+	sql := `SELECT column_name FROM information_schema.columns
+		WHERE table_name=$1 order by ordinal_position`
+	result, err := pc.conn.Query(sql, tableName)
+	if err != nil {
+		return err
+	}
+
+	for result.Next() {
+		var columnName string
+		if err = result.Scan(&columnName); err != nil {
+			return err
+		}
+		columnNames = append(columnNames, columnName)
+	}
+
+	deleteSql := fmt.Sprintf("DELETE FROM %s WHERE", tableName)
+	for _, row := range re.Rows {
+		for i, column := range row {
+			var condition string
+			switch column.(type) {
+			case string:
+				condition = fmt.Sprintf(" %v = '%v'", columnNames[i], column)
+			default:
+				condition = fmt.Sprintf(" %v = %v", columnNames[i], column)
+			}
+			deleteSql += condition
+
+			if i != len(row)-1 {
+				deleteSql += " AND"
+			}
+		}
+	}
+
+	_, err = pc.conn.Exec(deleteSql)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
